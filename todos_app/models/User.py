d73d32503016d3b163deb2ec6933694805328ae5
Alfredo08 <alfredosv08@gmail.com>
2021-09-29 16:48:56 -0500

    Adding INSERT and DELETE methods

--- todos_app/models/User.py
+++ todos_app/models/User.py
@@ -5,8 +5,35 @@ class User:
         self.username = username
         self.password = password
     
+    def printInfo( self ):
+        print(f"username: {self.username} password: {self.password} ")
+
     @classmethod
     def get_all_users( cls ):
         query = "SELECT * FROM users;"
         results = connectToMySQL( "todos_db" ).query_db( query )
-        return results
+
+        users = []
+        for user in results:
+            users.append( User( user['username'], user['password']) )
+        
+        return users
+    
+    @classmethod
+    def add_user( cls, newUser ):
+        query = "INSERT INTO users(username,password) VALUES(%(username)s, %(password)s);"
+        data = {
+            "username" : newUser.username,
+            "password" : newUser.password
+        }
+        result = connectToMySQL( "todos_db" ).query_db( query, data )
+        return result
+    
+    @classmethod
+    def delete_user( cls, username ):
+        query = "DELETE FROM users WHERE username=%(username)s"
+        data = {
+            "username": username
+        }
+        result = connectToMySQL( "todos_db" ).query_db( query, data )
+        return( result )
